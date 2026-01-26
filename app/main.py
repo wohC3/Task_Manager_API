fa3241ddb2d6b5cddc1dc5eebc8549ad3e78fe31
wohC3 <220244608+wohC3@users.noreply.github.com>
2026-01-26 14:48:57 +0200

    Created simple database and models files and modified main to create db on startup.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from .database import create_db_and_tables, SessionDep
+from .models import Task
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    create_db_and_tables() 
+    yield 
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 def root():
-    return{"message":"Task manager API is"}
+    return{"message":"Task manager API is running"}
---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlmodel import Field, SQLModel
+
+class Task(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+    description: str | None = None
+    completed: bool = False
